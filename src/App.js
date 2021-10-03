0c6a27a7d493313eea3b0109c1a8a59b9fbd7e98
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-10-04 02:16:55 +0600

    install react router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
+import Courses from './components/Courses/Courses';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-    <Navbar bg="dark" variant="dark">
-    <Container>
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-    <Nav className="me-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-    </Nav>
-    </Container>
-  </Navbar>
+    <BrowserRouter>
+      <Link to="/home">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/courses">Courses</Link>
+      <Switch>
+        <Route path='/home'>
+          <Home></Home>
+        </Route>
+        <Route path='/about'>
+          <About></About>
+        </Route>
+        <Route path='/courses'>
+          <Courses></Courses>
+        </Route>
+      </Switch>
+  </BrowserRouter>
+    
     </div>
   );
 }
