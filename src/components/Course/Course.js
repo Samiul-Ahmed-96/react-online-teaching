44f7b23236144742575c623e10babc62d781f7a3
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-10-04 17:31:18 +0600

    Display courses data

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Card, Col } from 'react-bootstrap';
 
 const Course = (props) => {
     const {name,img,description,price,rating} = props.course;
     return (
         <div>
-            
+        <Col>
+        <Card>
+          <Card.Img variant="top" src={img} />
+          <Card.Body>
+            <Card.Title>{name}</Card.Title>
+            <Card.Text>
+              {description}
+            </Card.Text>
+            <h4>{rating}</h4>
+            <h2>Price:${price}</h2>
+          </Card.Body>
+        </Card>
+      </Col>
         </div>
     );
 };
--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Container, Row } from 'react-bootstrap';
 import Course from '../Course/Course';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
 
 const Courses = () => {
     const [courses,setCourses] = useState([]);
@@ -11,13 +13,17 @@ const Courses = () => {
         .then(data => setCourses(data))
     },[])
     return (
+        <>
+        <Header></Header>
         <Container>
-        <Row xs={1} md={2} className="g-4">
+        <Row xs={1} md={3} className="g-4">
             {
                 courses.map(course => <Course course={course}></Course>)
             }
         </Row>
+        <Footer></Footer>
         </Container>
+        </>
     );
 };
 
